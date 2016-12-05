d695d8c861a5b731d32151a4cc4801a9ca0598ef
mohammad4x <m.ferdosi94@gmail.com>
2016-12-05 23:25:29 +0330

    Fixing Writing Styles

--- actions/Monitor/monitorList.js
+++ actions/Monitor/monitorList.js
@@ -1,69 +1,47 @@
-
-
-var Input={
-
-  accountHashID:
-  {
+var Input = {
+	accountHashID: {
     required: true
-    validator: function(value , connection , actionTemplate){}
-    default: function(value , connection , actionTemplate){return 0}
+    validator: function(value , connection , actionTemplate) {}
+    default: function(value , connection , actionTemplate) {return 0}
   },
-
-  statusCode:
-  {
+	statusCode: {
     required: false
-    validator: function(value , connection , actionTemplate){}
-    default: function(value , connection , actionTemplate){return 0}
+    validator: function(value , connection , actionTemplate) {}
+    default: function(value , connection , actionTemplate) {return 0}
   },
-
-  action:
-  {
+	action: {
     required: false
-    validator: function(value , connection , actionTemplate){}
-    default: function ( value , connection , actionTemplate){return 0}
+    validator: function(value , connection , actionTemplate) {}
+    default: function ( value , connection , actionTemplate) {return 0}
   },
-
-  serviceCaller:
-  {
+	serviceCaller: {
     required: false
-    validator: function(value , connection , actionTemplate){}
-    default: function(value , connection , actionTemplate){return 0}
+    validator: function(value , connection , actionTemplate) {}
+    default: function(value , connection , actionTemplate) {return 0}
   },
-
-  moduleCaller:
-  {
+	moduleCaller: {
     required: false
-    validator: function(value , connection , actionTemplate){}
-    default: function(value , connection , actionTemplate){return 0}
+    validator: function(value , connection , actionTemplate) {}
+    default: function(value , connection , actionTemplate) {return 0}
   }
-
-
 }
-
-
 exports.monitorList = {
   name = 'monitorList',
   description = 'Get monitor List',
   inputs : Input,
-
-  run: function(api , data , next)
-  {
+	run: function(api , data , next) {
     var filter = {
         statusCode: data.params.statusCode,
         action: data.params.action,
         serviceCaller: data.params.serviceCaller,
         moduleCaller: data.params.moduleCaller
-
-    }
-
+      }
     monitorList(data.params.accountHashID, filter, function (error, result) {
-      if (error)
-      {
+      if (error) {
         data.response.error = error.error
         next(error)
       }
-      else
-      {
+      else {
         data.response.result = result.result
         next()
       }})
