21eb1833ff0078302c8c116364bebf63ac78d5b7
mohammad4x <m.ferdosi94@gmail.com>
2016-12-05 23:17:33 +0330

    Fixing Writing Styles

--- actions/Monitor/monitorModelDelete.js
+++ actions/Monitor/monitorModelDelete.js
@@ -1,32 +1,25 @@
 
-var Input =
-{
-  accountHashID:
-  {
+var Input = {
+  accountHashID: {
     required : true
   },
-  monitorHashID:
-  {
+  monitorHashID: {
     required: true
   }
 }
-
-exports.monitorModelDelete =
-{
+exports.monitorModelDelete = {
   name: 'monitorModelDelete',
   description: 'Delete Monitor Model',
   inputs: Input,
-
-  run: function(api, data, next)
-  {
+  run: function(api, data, next) {
     monitorModelDelete(data.accountHashID, data.monitorHashID, function (error, result) {
       if (error) {
-        data.response.error = error.error;
-        next(error);
+        data.response.error = error.error
+        next(error)
       }
       else {
-        data.response.result = result.result;
-        next();
+        data.response.result = result.result
+        next()
       }
     })
   }
