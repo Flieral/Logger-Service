5617dde9e575e11eb1ba3cf69ade2c2bacaa97c7
mohammad4x <m.ferdosi94@gmail.com>
2016-12-05 23:11:19 +0330

    Fixing Writing Styles

--- actions/Monitor/monitorListDelete.js
+++ actions/Monitor/monitorListDelete.js
@@ -1,42 +1,32 @@
-var Input =
-{
-  accountHashID:
-  {
+var Input = {
+  accountHashID: {
     required : true
   },
-  statusCode:
-  {
+  statusCode: {
     required : false,
     validator: function(value , connection, actionTemplate){},
     default : function(value , connection, actionTemplate){}
   },
-  action:
-  {
+  action: {
     required : false,
     validator: function(value , connection, actionTemplate){},
     default : function(value , connection, actionTemplate){}
   },
-  serviceCaller:
-  {
+  serviceCaller: {
     required : false,
     validator: function(value , connection, actionTemplate){},
     default : function(value , connection, actionTemplate){}
   },
-  moduleCaller:
-  {
+  moduleCaller: {
     required : false,
     validator: function(value , connection, actionTemplate){},
     default : function(value , connection, actionTemplate){}
   }
 }
-
-
-exports.monitorListDelete =
-{
+exports.monitorListDelete = {
   name: 'monitorListDelete',
   description: 'Delete Monitor Model List',
   inputs: Input,
-
   run: function (api, data, next) {
     var filter = {
       statusCode: data.params.statusCode,
@@ -44,15 +34,14 @@ exports.monitorListDelete =
       serviceCaller: data.params.serviceCaller,
       moduleCaller: data.params.moduleCaller
     }
-
-    monitorListDelete(data.params.accountHashID, filter, function (error, result) {
+	monitorListDelete(data.params.accountHashID, filter, function (error, result) {
       if (error) {
-        data.response.error = error.error;
-        next(error);
+        data.response.error = error.error
+        next(error)
       }
       else {
-        data.response.result = result.result;
-        next();
+        data.response.result = result.result
+        next()
       }
     })
   }
