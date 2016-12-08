5654fdb5821e26951e0219fc3a736326cb9467ce
jigser <rafiee.shervin@gmail.com>
2016-12-08 20:26:00 +0330

    add start engine function

--- logic/payloadChecker.js
+++ logic/payloadChecker.js
@@ -2,8 +2,9 @@ var payloadCheck = require('../config/payloadCheck.json')
 var config = require('../config/configuration.json')
 
 module.exports = {
+
   requiredCheck: function(body, callback){
-    var peyKeys = Object.keys(payloadCheck)
+    var peyKeys = Object.keys(payloadCheck.monitorModelEntry)
     for (var i = 0; i < peyKeys.length; i++){
       if (payloadCheck[peyKeys[i]].required && !body[peyKeys[i]]){
         callback(New Error(peyKeys[i] + config.message.keyRequiredError))
@@ -15,23 +16,36 @@ module.exports = {
     var bodyKeys = Object.keys(body)
     for (var i = 0; i < bodyKeys.length; i++)
     {
-      switch (payloadCheck[bodyKeys[i]].type)
+      switch (payloadCheck.monitorModelEntry.[bodyKeys[i]].type)
       {
         case "int":
-        callback(null, parseInt(body[bodyKeys[i]]))
+        body[bodyKeys[i] = parseInt(body[bodyKeys[i]]))
+        break
         case "double":
-        callback(null, parseFloat(body[bodyKeys[i]]))
-        default:
-        callback(null, body[bodyKeys[i]])
+        body[bodyKeys[i] = parseFloat(body[bodyKeys[i]]))
+        break
       }
     }
+    callback(null,body)
   },
 
   validator: function(body, callback){
     var bodyKeys = Object.keys(body)
     for (var i = 0; i < bodyKeys.length; i++){
-      if (!payloadCheck[bodyKeys[i]])
+      if (!payloadCheck.monitorModelEntry.[bodyKeys[i]])
       callback(New Error(bodyKeys[i] + "Key does not exist"), null)
     }
+  }
+
+  startChecking: function(body, finalCallback){
+    this.requiredCheck(body, func(error){
+      finalcallback(error, null)
+    })
+    this.validator(body, func(error){
+      finalcallback(error, null)
+    })
+    this.formatter(body, func(error, result){
+      finalcallback(error, result)
+    })
   }
 }
