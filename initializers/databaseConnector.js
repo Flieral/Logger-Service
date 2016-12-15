b97769e15628b33be080385104e99b8ed3a028f4
mohammad4x <m.ferdosi94@gmail.com>
2016-12-15 16:25:38 +0330

    Fix Some Types and Scripts and Delete Unnecessary Test File

--- initializers/databaseConnector.js
+++ initializers/databaseConnector.js
@@ -2,19 +2,19 @@ var redis = require("../public/redisClient")
 
 module.exports =
 {
-	start: function(api, next)
-	{
-		api.redisClient = {};
+  start: function(api, next)
+  {
+    api.redisClient = {};
 
-		api.log('[DATABASE][REDIS][START]', 'info')
-		api.redisClient = redis.startRedisClient()
-		next()
-	},
+    api.log('[DATABASE][REDIS][START]', 'info')
+    api.redisClient = redis.startRedisClient()
+    next()
+  },
 
-	stop: function(api, next)
-	{
-		api.log('[DATABASE][REDIS][STOP]', 'info')
-		api.redisClient.stopRedisClient()
-		next()
-	}
+  stop: function(api, next)
+  {
+    api.log('[DATABASE][REDIS][STOP]', 'info')
+    redis.stopRedisClient()
+    next()
+  }
 }
