2519fb7c979a3b500f089c21fe4d83cb26c649f7
MrWooJ <hurricanc@gmail.com>
2016-12-21 23:47:30 +0330

    Replace -Inf by 0 and +Inf by -1

--- logic/monitorListLogic.js
+++ logic/monitorListLogic.js
@@ -7,7 +7,7 @@ module.exports = function(redisClient, accountHashID, filter, callback) {
   var monitorModelTables = Object.keys(configuration.TableMonitorModel)
   if(filterKeys.length == 0) {
     var tableName = configuration.TableMSAccountModelMonitorModel + accountHashID
-    redisClient.zrangebyscore(tableName, '-inf', '+inf' , 'WITHSCORE', function(err , replies) {
+    redisClient.zrangebyscore(tableName, '0', '-1' , 'WITHSCORES', function(err , replies) {
       if(err)
       callback(err, null)
       callback(null, replies)
@@ -27,7 +27,7 @@ module.exports = function(redisClient, accountHashID, filter, callback) {
     redisClient.zinterstore(args, function(err, replies) {
       if (err)
       callback(err, null)
-      redisClient.zrangebyscore(tableName, '0', '-1' , 'WITHSCORE', function(err , replies) {
+      redisClient.zrangebyscore(tableName, '0', '-1' , 'WITHSCORES', function(err , replies) {
         if(err)
         callback(err, null)
         result = replies
