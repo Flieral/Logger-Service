aaf89722894478e2d27aec6f2b81511c69ad754c
jigser <rafiee.shervin@gmail.com>
2016-12-22 19:02:59 +0330

    Add Testcase for Showing Logs JSON

---
+++ test/monitorModelTest.js
@@ -0,0 +1,52 @@
+'use strict'
+var request = require('request')
+var fs = require('fs')
+var os = require('os')
+var should = require('should')
+var utility = require('../public/utility')
+let path = require('path')
+var api
+var url
+
+describe('Server: Web', function () {
+
+  before(function (done) {
+    url = 'http://localhost:8085/api'
+    done()
+  })
+
+  after(function (done) {
+    done()
+  })
+
+  var resres
+  var Body = {accountHashID: 'mohammad', statusCode: 'Sample2', serviceCaller: 'Sample1', moduleCaller: 'Sample1', actionType:'Sample1', time:1234567890, logMessage:'this is success', objectInfo:'this object has not f ing info!!'}
+  it ('HTTP Verbs should work: Post with JSON Payload as body', function(done) {
+
+    request.post(url + '/1/monitor', {'body': JSON.stringify(Body), 'headers': {'Content-type': 'application/json'}}, function (error, response, body) {
+      if (error) {
+        console.log(error)
+        should.not.exist(error)
+      }
+      body = JSON.parse(body)
+      if (body.result) {
+        resres = body.result.monitorHashID
+        done()
+      }
+    })
+  })
+
+  it ('Get with accountHashID', function(done) {
+    request.get(url + '/1/monitor/' + resres + '?accountHashID=mohammad', function (error, response, body) {
+      if (error){
+        console.log(error)
+        should.not.exist(error)
+      }
+      body = JSON.parse(body)
+      body.result.accountHashID = 'mohammad'
+      if (utility.objectCompare(body.result , Body)) {
+        done()
+      }
+    })
+  })
+})
