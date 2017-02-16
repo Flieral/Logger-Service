1ef86a69fb010feea5a681829867821d9b8f298f
MrWooJ <hurricanc@gmail.com>
2017-02-16 11:51:33 +0330

    Fix Rotues by RESTify Rules

--- config/routes.js
+++ config/routes.js
@@ -4,11 +4,11 @@ exports.default = {
 			get: [
 				/* URIs */
 				{
-					path: '/:apiVersion/account/:accountHashId/monitor',
+					path: '/:apiVersion/account/:accountHashID/monitor',
 					action: 'monitorList'
 				},
 				{
-					path: '/:apiVersion/account/:accountHashId/monitor/:monitorHashID',
+					path: '/:apiVersion/account/:accountHashID/monitor/:monitorHashID',
 					action: 'monitorModel'
 				},
 
@@ -22,18 +22,18 @@ exports.default = {
 			post: [
 				/* URIs */
 				{
-					path: '/:apiVersion/account/:accountHashId/monitor',
+					path: '/:apiVersion/account/:accountHashID/monitor',
 					action: 'monitorModelEntry'
 				}
 			],
 			delete: [
 				/* URIs */
 				{
-					path: '/:apiVersion/account/:accountHashId/monitor',
+					path: '/:apiVersion/account/:accountHashID/monitor',
 					action: 'monitorListDelete'
 				},
 				{
-					path: '/:apiVersion/account/:accountHashId/monitor/:monitorHashID',
+					path: '/:apiVersion/account/:accountHashID/monitor/:monitorHashID',
 					action: 'monitorModelDelete'
 				}
 			]
